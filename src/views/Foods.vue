86fc2fdc42c4e72105208ea8974eeaed4e277a95
nuzululm <nuzul.sealovers@gmail.com>
2022-07-07 08:31:00 +0700

    first commit

---
+++ src/views/Foods.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="foods">
+        <Navbar />
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col">
+                    <h2>Daftar <strong>Makananan</strong></h2>
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <div class="col">
+                    <div class="input-group mb-3">
+                        <input
+                            v-model="search"
+                            type="text"
+                            class="form-control"
+                            placeholder="Cari Makanan Kesukaan Anda ..."
+                            aria-label="Cari Makanan Kesukaan Anda ..."
+                            aria-describedat="basic-addon1"
+                            @keyup="searchFood"
+                        />
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">
+                                <b-icon-search></b-icon-search>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <div
+                    class="col-md-4 mt-4"
+                    v-for="product in products"
+                    :key="product.id"
+                >
+                    <CardProduct :product="product" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
+
+export default {
+    name: "Foods",
+    components: {
+        Navbar,
+        CardProduct,
+    },
+    data() {
+        return {
+            products: [],
+            search : '',
+        };
+    },
+    methods: {
+        setProduct(data) {
+            this.products = data;
+        },
+        searchFood(){
+            axios
+            .get("http://localhost:3000/products?q="+this.search)
+            .then((response) => {
+                //handle success
+                this.setProduct(response.data);
+            })
+            .catch((error) => {
+                //handle error
+                console.log("Gagal : ", error);
+            });
+        }
+    },
+    mounted() {
+        axios
+            .get("http://localhost:3000/products")
+            .then((response) => {
+                //handle success
+                this.setProduct(response.data);
+            })
+            .catch((error) => {
+                //handle error
+                console.log("Gagal : ", error);
+            });
+    },
+};
+</script>
